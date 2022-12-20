604c8f3ee545085b7b663e132604a800f421232b
djkazunoko <djkazunoko@gmail.com>
2022-12-21 08:23:55 +0900

    データをLocalStorageに保存

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 let id = 0;
+const STORAGE_KEY = "vue-todoapp";
 
 createApp({
   data() {
     return {
       newTodo: "",
-      todos: [],
+      todos: JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]"),
       editedTodo: null,
     };
   },
   methods: {
     addTodo() {
       this.todos.push({ id: id++, text: this.newTodo });
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
       this.newTodo = "";
     },
     removeTodo(todo) {
       this.todos = this.todos.filter((t) => t !== todo);
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
     },
     editTodo(todo) {
       this.editedTodo = todo;
@@ -24,6 +27,7 @@ createApp({
     doneEdit(todo) {
       this.editedTodo = null;
       todo.text = todo.text.trim();
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
       if (!todo.text) {
         this.removeTodo(todo);
       }
