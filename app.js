e2e8307e714c461a534c4d1827ddd36c1b5cf82c
djkazunoko <djkazunoko@gmail.com>
2022-12-20 09:19:37 +0900

    ToDoの登録と一覧表示

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
+let id = 0;
+
 createApp({
   data() {
     return {
-      text: "test",
+      newTodo: "",
+      todos: [],
     };
   },
+  methods: {
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo });
+      this.newTodo = "";
+    }
+  }
 }).mount("#app");
