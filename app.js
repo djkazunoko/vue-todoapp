09e9857df79c1cdac57004262e97177864d2b9a3
djkazunoko <djkazunoko@gmail.com>
2022-12-21 14:01:23 +0900

    ToDoの処理状態ごとの表示

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 let id = 0;
 const STORAGE_KEY = "vue-todoapp";
+const filters = {
+  all: (todos) => todos,
+  active: (todos) => todos.filter((todo) => !todo.done),
+  done: (todos) => todos.filter((todo) => todo.done),
+};
 
 createApp({
   data() {
@@ -9,7 +14,13 @@ createApp({
       newTodo: "",
       todos: JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]"),
       editedTodo: null,
+      visibility: "all",
     };
+  },
+  computed: {
+    filteredTodos() {
+      return filters[this.visibility](this.todos);
+    },
   },
   methods: {
     addTodo() {
@@ -31,6 +42,9 @@ createApp({
       if (!todo.text) {
         this.removeTodo(todo);
       }
+    },
+    changeVisibility(visibility) {
+      this.visibility = visibility;
     },
   },
 }).mount("#app");
